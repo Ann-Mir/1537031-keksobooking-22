b8ddd5d0f5bb920354d5f1637c6c69961bde6ae0
Anna Mironovich <anna.mail787@gmail.com>
2021-03-05 16:13:13 +0300

    filter

--- js/filter.js
+++ js/filter.js
@@ -1,8 +1,14 @@
-import { removeMapMarkers, renderCards } from './map.js';
+/* global _:readonly */
+
+import {removeMapMarkers, renderCards} from './map.js';
 
 const filterForm = document.querySelector('.map__filters');
 const filterElements = filterForm.elements;
 const housingTypeSelect = filterForm.querySelector('#housing-type');
+const housingPriceSelect = filterForm.querySelector('#housing-price');
+const housingRoomsSelect = filterForm.querySelector('#housing-rooms');
+const housingGuestsSelect = filterForm.querySelector('#housing-guests');
+
 
 const deactivateFilter = () => {
   filterForm.classList.add('.map__filters--disabled');
@@ -18,28 +24,71 @@ const activateFilter = () => {
   }
 }
 
-const filterAdvertisements = (advertisements) => {
+const checkType = (advertisement) => {
+  return housingTypeSelect.value === 'any' || advertisement.offer.type === housingTypeSelect.value;
+};
 
-  const onFilterByTypeChange = (advertisements) => {
-
-    return (evt) => {
-      evt.preventDefault();
-      const type = housingTypeSelect.value;
-      if (type === 'any') {
-        removeMapMarkers();
-        renderCards(advertisements);
-        return;
-      }
-      const filteredAdvertisements = advertisements.filter((advertisement) => {
-        return advertisement.offer.type === type;
-      });
-      removeMapMarkers();
-      renderCards(filteredAdvertisements);
-    }
+const checkPrice = (advertisement) => {
+  const priceOptions = {
+    'any': () => true,
+    'low': (price) => price < 10000,
+    'middle': (price) => price >= 10000 && price < 50000,
+    'high': (price) => price >= 50000,
   }
-
-  housingTypeSelect.addEventListener('change', onFilterByTypeChange(advertisements));
+  return priceOptions[housingPriceSelect.value](advertisement.offer.price);
 }
 
+const checkRooms = (advertisement) => {
+  return housingRoomsSelect.value === 'any' || Number(housingRoomsSelect.value) === advertisement.offer.rooms;
+}
 
-export { deactivateFilter, activateFilter, filterForm, filterAdvertisements }
+const checkGuests = (advertisement) => {
+  const guestOptions = {
+    'any': () => true,
+    '0': (guestsCount) => guestsCount === 100,
+    '1': (guestsCount) => guestsCount === 1,
+    '2': (guestsCount) => guestsCount === 2,
+    '3': (guestsCount) => guestsCount === 3,
+  }
+  return guestOptions[housingGuestsSelect.value](advertisement.offer.guests);
+}
+
+const checkFeatures = (advertisement) => {
+  const checkedFeatures = filterForm.querySelectorAll('.map__checkbox:checked');
+  let count = 0;
+
+  checkedFeatures.forEach((feature) => {
+    if (advertisement.offer.features.includes(feature.value))
+      count++;
+  })
+
+  return count === checkedFeatures.length;
+}
+
+const getFilteredAds = (advertisements) => {
+  const filteredAdvertisements = advertisements.filter ((advertisement) => {
+    return (
+      checkType(advertisement) &&
+      checkPrice(advertisement) &&
+      checkRooms(advertisement) &&
+      checkGuests(advertisement) &&
+      checkFeatures(advertisement)
+    )
+  })
+  return filteredAdvertisements;
+}
+
+const onFilterChange = (advertisements) => {
+  return (evt) => {
+    evt.preventDefault();
+    const filteredAdds = getFilteredAds(advertisements);
+    removeMapMarkers();
+    renderCards(filteredAdds);
+  }
+}
+
+const setFilterChange = (advertisements) => {
+  filterForm.addEventListener('change', onFilterChange(advertisements));
+};
+
+export { deactivateFilter, activateFilter, filterForm, setFilterChange }
